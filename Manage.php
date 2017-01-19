b63537c27d48f2bcb2de15c1c207a291fa82ebdf
Magnetite <spaceman389@yahoo.com>
2017-01-19 10:23:14 -0600

    Added buttons to Manage.php

--- Manage.php
+++ Manage.php
@@ -2,8 +2,33 @@
 <html>
 <head>
 <title> Manage Database</title>
+
+<link rel="stylesheet" 
+href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+
 </head>
 <body>
-<button><a href="Test_POS.php">Back to Register</a></button>
+<div class="btn btn-default"><a href="Test_POS.php">Back to Register</a></div>
+
+<?php echo 3**5 ?>
+
+<div class="container">
+	<div class="row">
+	<div class="col-md-9">
+	<div class="btn-group-vertical">
+	
+	<div class="btn btn-default">Change Row of Table</div>
+	<div class="btn btn-default">Delete Row of Table</div>
+	<div class="btn btn-default">Add Row of Table</div>
+	<div class="btn btn-default">Show Row of Table</div>
+	<div class="btn btn-default">Show All Rows of Table</div>
+	
+	
+	
+	</div>
+	</div>
+	</div>
+	</div>
 </body>
 </html>
