045eaaee82712bcb212144ce730cd25a3df47278
Magnetite <spaceman389@yahoo.com>
2017-01-20 21:07:47 -0600

    Added Javascript Libraries

--- Manage.php
+++ Manage.php
@@ -2,10 +2,13 @@
 <html>
 <head>
 <title> Manage Database</title>
+<meta charset="utf-8">
 
 <link rel="stylesheet" 
 href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+<script src="JSPOS.js"></script>
 
 </head>
 <body>
@@ -19,9 +22,9 @@ href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 	<div class="btn-group-vertical">
 	
 	<div class="btn btn-default">Change Row of Table</div>
-	<div class="btn btn-default">Delete Row of Table</div>
-	<div class="btn btn-default">Add Row of Table</div>
-	<div class="btn btn-default">Show Row of Table</div>
+	<div class="btn btn-default" id="DeleteRow">Delete Row of Table</div>
+	<div class="btn btn-default" id="CreateRow" >Add Row of Table</div>
+	<div class="btn btn-default" id="ReadRow">Show Row of Table</div>
 	<div class="btn btn-default">Show All Rows of Table</div>
 	
 	
