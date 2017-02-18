7e2cad9508ee099772b30ee800bb60cb4964ebb2
Magnetite <spaceman389@yahoo.com>
2017-02-17 23:17:29 -0600

    Added Receipt Page
    The Receipt page will be the printable receipt for the sale.

---
+++ Receipt.php
@@ -0,0 +1,26 @@
+<DOCTYPE! html>
+<html>
+<head>
+<meta charset="utf-8">
+
+<title> 
+POS Receipt
+</title>
+
+<link rel="stylesheet" 
+href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+<script src="JSPOS.js"></script>
+
+
+</head>
+
+<body>
+
+<div class="jumbotron"><h1>Receipt Page</h1></div>
+
+</body>
+</html>
+
+
