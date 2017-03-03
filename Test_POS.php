a6e876194c9f8dd1765d1b7a877da23fac6e0e61
Magnetite <spaceman389@yahoo.com>
2017-03-02 23:38:26 -0600

    Changed the UI button style to primary

--- Test_POS.php
+++ Test_POS.php
@@ -34,39 +34,40 @@ href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 		
 		
 			<div class="btn-group-vertical">
-				<div class="denom btn btn-default" id="50d"> $50</div>
-				<div class="denom btn btn-default" id="20d"> $20</div>
-				<div class="denom btn btn-default" id="10d"> $10</div>
-				<div class="denom btn btn-default" id="5d"> $5</div>
-				<div class="denom btn btn-default" id="1d"> $1</div>
+				<div class=" btn btn-primary" id="50d"> $50</div>
+				<div class=" btn btn-primary" id="20d"> $20</div>
+				<div class=" btn btn-primary" id="10d"> $10</div>
+				<div class=" btn btn-primary" id="5d"> $5</div>
+				<div class=" btn btn-primary" id="1d"> $1</div>
 			</div>
 		
 			<div class="btn-group-vertical">
-			<div class="btn btn-default denom" id="cash" >Cash</div> 
-			<div class="btn btn-default denom" id="exact" >Exact</div>
+			<div class="btn btn-primary " id="cash" >Cash</div> 
+			<div class="btn btn-primary " id="exact" >Exact</div>
 			<br>
-			<div class="btn btn-default denom" id="NearestD" >Nearest 1.00</div> 
-			<div class="btn btn-default denom" id="Nearest25" >Nearest .25</div> 
-			<div class="btn btn-default denom" id="Nearest10" >Nearest .10</div> 
-			<div class="btn btn-default denom" id="Nearest5" >Nearest .05</div>
+			<div class="btn btn-primary " id="NearestD" >Nearest 1.00</div> 
+			<div class="btn btn-primary " id="Nearest25" >Nearest .25</div> 
+			<div class="btn btn-primary " id="Nearest10" >Nearest .10</div> 
+			<div class="btn btn-primary " id="Nearest5" >Nearest .05</div>
 			<br>
-			<div class="btn btn-default denom" id="Dup" >Quantity</div>
+			<div class="btn btn-primary " id="Dup" >Quantity</div>
 			<br>
-			<div class="btn btn-default denom" id="More50" >$50 More</div> 
-			<div class="btn btn-default denom" id="More20" >$20 More</div> 
-			<div class="btn btn-default denom" id="More10" >$10 More</div> 
-			<div class="btn btn-default denom" id="More5" >$5 More</div> 
-			<div class="btn btn-default denom" id="More1" >$1 More</div> 
+			<div class="btn btn-primary " id="More50" >$50 More</div> 
+			<div class="btn btn-primary " id="More20" >$20 More</div> 
+			<div class="btn btn-primary " id="More10" >$10 More</div> 
+			<div class="btn btn-primary " id="More5" >$5 More</div> 
+			<div class="btn btn-primary " id="More1" >$1 More</div> 
 			<br>
-			<div class="btn btn-default denom" id="print" >Print Receipt</div>
-			<div class="btn btn-default denom" id="done" >Done</div>
+			<div class="btn btn-primary " id="print" >Print Receipt</div>
+			<div class="btn btn-primary " id="done" >Done</div>
 			
 			
-			<div class="btn btn-default denom" > <a href="Manage.php">Manage DB</a></div>
-			<div class="btn btn-default denom" id="adjust" >Adjust Price</div>
+			<div class="btn btn-primary " > <a href="Manage.php">Manage DB</a></div>
+			<div class="btn btn-primary " id="adjust" >Adjust Price</div>
 			</div>
 			<div class="btn-group-vertical" id = "menuButtons">
 			</div>
+			
 			
 		</div>
 		
@@ -88,9 +89,9 @@ href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 	
 </div>
 <br />
-<div id="nav" class="btn btn-default">Back to register</div> <div id="printer" class="btn btn-default">print</div>
+<div id="nav" class="btn btn-primary">Back to register</div> <div id="printer" class="btn btn-primary">print</div>
 <br />
-<div id="NextOrder" class="btn btn-default">Next Order</div>
+<div id="NextOrder" class="btn btn-primary">Next Order</div>
 <br />
 <table id="output"></table>
 
