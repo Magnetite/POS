d844b6c623eed3e9e18aeec0bbdd920123ba1b3b
Magnetite <spaceman389@yahoo.com>
2017-03-03 19:49:50 -0600

    Updated the UI, button groupings

--- Test_POS.php
+++ Test_POS.php
@@ -30,48 +30,49 @@ href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 		
 
 		
-		<div class="col-md-6">
+		<div class="col-md-7 col-sm-7 col-xs-7">
 		
 		
-			<div class="btn-group-vertical">
-				<div class=" btn btn-primary" id="50d"> $50</div>
-				<div class=" btn btn-primary" id="20d"> $20</div>
-				<div class=" btn btn-primary" id="10d"> $10</div>
-				<div class=" btn btn-primary" id="5d"> $5</div>
+			<div class="btn-group btn-group-lg">
 				<div class=" btn btn-primary" id="1d"> $1</div>
+				<div class=" btn btn-primary" id="5d"> $5</div>
+				<div class=" btn btn-primary" id="10d"> $10</div>
+				<div class=" btn btn-primary" id="20d"> $20</div>
+				<div class=" btn btn-primary" id="50d"> $50</div>
 			</div>
 		
-			<div class="btn-group-vertical">
-			<div class="btn btn-primary " id="cash" >Cash</div> 
-			<div class="btn btn-primary " id="exact" >Exact</div>
-			<br>
-			<div class="btn btn-primary " id="NearestD" >Nearest 1.00</div> 
-			<div class="btn btn-primary " id="Nearest25" >Nearest .25</div> 
-			<div class="btn btn-primary " id="Nearest10" >Nearest .10</div> 
-			<div class="btn btn-primary " id="Nearest5" >Nearest .05</div>
-			<br>
-			<div class="btn btn-primary " id="Dup" >Quantity</div>
-			<br>
-			<div class="btn btn-primary " id="More50" >$50 More</div> 
-			<div class="btn btn-primary " id="More20" >$20 More</div> 
-			<div class="btn btn-primary " id="More10" >$10 More</div> 
-			<div class="btn btn-primary " id="More5" >$5 More</div> 
-			<div class="btn btn-primary " id="More1" >$1 More</div> 
-			<br>
-			<div class="btn btn-primary " id="print" >Print Receipt</div>
-			<div class="btn btn-primary " id="done" >Done</div>
-			
-			
-			<div class="btn btn-primary " > <a href="Manage.php">Manage DB</a></div>
-			<div class="btn btn-primary " id="adjust" >Adjust Price</div>
+			<div class="btn-group btn-group-lg">
+				<div class="btn btn-primary " id="Nearest5" >Nearest .05</div>
+				<div class="btn btn-primary " id="Nearest10" >Nearest .10</div> 
+				<div class="btn btn-primary " id="Nearest25" >Nearest .25</div> 
+				<div class="btn btn-primary " id="NearestD" >Nearest 1.00</div> 
 			</div>
+			
+			<div class="btn-group btn-group-lg">
+				<div class="btn btn-primary " id="More1" >$1 More</div> 
+				<div class="btn btn-primary " id="More5" >$5 More</div>
+				<div class="btn btn-primary " id="More10" >$10 More</div>
+				<div class="btn btn-primary " id="More20" >$20 More</div>
+				<div class="btn btn-primary " id="More50" >$50 More</div> 
+			</div>
+			
+			<div class="btn-group-vertical btn-group-lg">
+				<div class="btn btn-primary " id="cash" >Cash</div> 
+				<div class="btn btn-primary " id="exact" >Exact</div>
+				<div class="btn btn-primary " id="Dup" >Quantity</div>
+				<div class="btn btn-primary " id="print" >Print Receipt</div>
+				<div class="btn btn-primary " id="done" >Done</div>		
+				<div class="btn btn-primary" > <a href="Manage.php">Manage DB</a></div>
+				<div class="btn btn-primary " id="adjust" >Adjust Price</div>
+			</div>
+			
 			<div class="btn-group-vertical" id = "menuButtons">
 			</div>
 			
 			
 		</div>
 		
-		<div class="receipt col-md-6">
+		<div class="receipt col-md-5 col-sm-5 col-xs-5">
 			<div id="list"></div>
 			<div id="subtotal" > Subtotal: 0</div>
 			<div id="tax" > Tax: 0</div>
