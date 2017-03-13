9bf30753c0179c5c858f09919d6011e14b778eee
Magnetite <spaceman389@yahoo.com>
2017-03-13 00:42:16 -0500

    Added icons to buttons on Manage.php

--- Manage.php
+++ Manage.php
@@ -14,7 +14,8 @@ href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 
 </head>
 <body>
-<a href="Test_POS.php" class="btn btn-default">Back to Register</a>
+<a href="Test_POS.php" class="btn btn-default">
+<span class="glyphicon glyphicon-arrow-left"></span> Back to Register</a>
 
 
 
@@ -24,15 +25,15 @@ href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 	<div class="btn-group-vertical">
 	
 	
-	<div class="btn btn-default" id="CreateRow" >Add Row of Table</div>
-	<div class="btn btn-default" id="ReadRow">Show Row of Table</div>
-	<div class="btn btn-default" id="UpdateRow">Change Row of Table</div>
-	<div class="btn btn-default" id="UpdateColumn">Update Column*</div>
-	<div class="btn btn-default" id="UpdateInven">Update Stock*</div>
-	<div class="btn btn-default" id="DeleteRow">Delete Row of Table</div>
+	<div class="btn btn-default" id="CreateRow" ><span class="glyphicon glyphicon-plus"></span> Add Row of Table</div>
+	<div class="btn btn-default" id="ReadRow"><span class="glyphicon glyphicon-search"></span>  Show Row of Table</div>
+	<div class="btn btn-default" id="UpdateRow"><span class="glyphicon glyphicon-option-horizontal"></span> Change Row of Table</div>
+	<div class="btn btn-default" id="UpdateColumn"><span class="glyphicon glyphicon-option-vertical"></span> Update Column*</div>
+	<div class="btn btn-default" id="UpdateInven"><span class="glyphicon glyphicon-upload"></span> Update Stock*</div>
+	<div class="btn btn-default" id="DeleteRow"><span class="glyphicon glyphicon-remove-sign"></span> Delete Row of Table</div>
 	
-	<div class="btn btn-default" id="ShowAll">Show All Rows of Table</div>
-	<div class="btn btn-default" id="Clear">Clear Screen</div>
+	<div class="btn btn-default" id="ShowAll"> <span class="glyphicon glyphicon-list"></span> Show All Rows of Table</div>
+	<div class="btn btn-default" id="Clear"><span class="glyphicon glyphicon-erase"></span> Clear Screen</div>
 	
 	
 	
